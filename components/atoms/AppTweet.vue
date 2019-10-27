a8dd044fa4cd105dbe4983a0696961de17e1361f
hishinuma616 <dnbwoodleaves3616@gmail.com>
2019-10-28 08:29:10 +0900

    残タスクメモ

---
+++ components/atoms/AppTweet.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="tweet__container">
+    <div class="">
+      アイコン
+    </div>
+    <div>
+      メイン
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      default: '',
+    },
+    id: {
+      type: String,
+      default: '',
+    }
+    icon: {
+      type: String,
+      default: '',
+    },
+    tweet: {
+      type: String,
+      default: '',
+    },
+  }
+}
+</script>
+<style lang='scss' scoped>
+</style>
